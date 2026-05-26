b2cdbb005cfd9bbfd5b08466626cac5cbcc2e59e
Edy Cu <edy.cu@live.com>
2026-05-26 23:04:58 +0700

    build: update package and tsconfig configurations

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom/jest-globals";
+
+// Mock HTMLCanvasElement getContext to prevent jsdom errors
+if (typeof window !== "undefined") {
+  HTMLCanvasElement.prototype.getContext = jest.fn(() => ({
+    clearRect: jest.fn(),
+    fillRect: jest.fn(),
+  })) as any;
+}
